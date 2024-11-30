0f115f7f9478b2812134ac2627c233cb50de2b56
Akhil P A <akhilpunchayilalakkat@gmail.com>
2024-11-30 06:33:06 -0800

    initial commit

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,91 @@
+'use client'
+import * as React from "react"
+
+import { SearchForm } from "@/components/search-form"
+import { VersionSwitcher } from "@/components/version-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+// This is sample data.
+const data = {
+  versions: ["Logout"],
+  navMain: [
+    {
+      title: "Getting Started",
+      url: "#",
+      items: [
+        {
+          title: "Basic Info",
+          url: "/basic-info",
+        },
+        {
+          title: "Education",
+          url: "/education",
+        },
+        {
+          title: "Experience",
+          url: "/experience",
+        },
+        {
+          title: "Projects",
+          url: "/projects",
+        },
+        {
+          title: "Certifications",
+          url: "/certifications",
+        },
+      ],
+    }
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const pathname = usePathname();
+  const pageTitle = pathname === "/" ? "Home" : pathname.slice(1).replace(/-/g, " ");
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <VersionSwitcher
+          versions={data.versions}
+          defaultVersion={data.versions[0]}
+        />
+        <SearchForm />
+      </SidebarHeader>
+      <SidebarContent>
+        {/* We create a SidebarGroup for each parent. */}
+        {data.navMain.map((item) => (
+          <SidebarGroup key={item.title}>
+            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {item.items.map((item) => {
+                  const isActive = pathname === item?.url;
+                  return(
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild isActive={isActive}>
+                        <Link href={item.url}>{item.title}</Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  )
+                })}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
---
+++ src/components/dashboard-layout.tsx
@@ -0,0 +1,49 @@
+'use client'
+import React from 'react'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from './ui/sidebar'
+import { AppSidebar } from './app-sidebar'
+import { Separator } from '@radix-ui/react-separator'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from './ui/breadcrumb'
+import { usePathname } from 'next/navigation'
+
+type DashBoardLayoutProps = {
+    children:React.ReactNode
+}
+
+const DashBoardLayout = ({children}:DashBoardLayoutProps) => {
+    
+  const pathname = usePathname();
+  const pageTitle = pathname === "/" ? "Home" : pathname.slice(1).replace(/-/g, " ");
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem className="hidden md:block">
+                                <BreadcrumbLink href="#">
+                                    Building Your Application
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator className="hidden md:block" />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>{pageTitle}</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                </header>
+                <div className="flex flex-1 flex-col gap-4 p-4">
+                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                        {children}
+                    </div>
+                    <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
+    )
+}
+
+export default DashBoardLayout
